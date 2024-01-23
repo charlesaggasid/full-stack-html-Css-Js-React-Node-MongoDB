137e3f3992ff8047e5871e523c6137f33138cff0
Charles Aggasid <charles.aggasid01@gmail.com>
2024-01-23 12:58:39 -0600

    adding home.ejs

--- 34-templating-demo/index.js
+++ 34-templating-demo/index.js
@@ -3,6 +3,13 @@ const app = express();
 const port = 3000
 
 
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+	res.render('home.ejs')
+})
+
+
 app.listen(port,() => {
 	
 	console.log("Listening on port " + port);
